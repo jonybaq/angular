97e72eaa21a06c8232674584c5db7883b867280d
Jonathan <jony_004@hotmail.com>
2021-10-29 16:39:45 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido a Angular';
+  curso:string = 'Curso de Spring con Angular';
+
+  profesor: string= "Jonathan Baquero";
 }
